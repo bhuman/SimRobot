bce4bb7bc1b6651e91794ce5e85cf05bed40adad
Thomas Röfer <Thomas.Roefer@dfki.de>
2020-05-07 09:54:14 +0200

    Avoid error message when used in my VM

--- Make/VS2019/generate.cmd
+++ Make/VS2019/generate.cmd
@@ -1,5 +1,8 @@
 @echo off
 pushd "%~dp0"
 cmake.exe -G "Visual Studio 16 2019" -A x64 -S ..\Common -B ..\..\Build\Windows\CMake
-if not exist SimRobot.sln mklink SimRobot.sln ..\..\Build\Windows\CMake\SimRobot.sln
+if exist SimRobot.sln goto :linkExists
+if exist SimRobot.sln.lnk goto :linkExists
+mklink SimRobot.sln ..\..\Build\Windows\CMake\SimRobot.sln
+:linkExists
 popd
