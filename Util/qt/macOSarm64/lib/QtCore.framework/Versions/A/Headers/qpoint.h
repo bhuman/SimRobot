89e311a445a3e7436406366c8bf6d65e28208693
Thomas Röfer <Thomas.Roefer@dfki.de>
2021-07-04 21:22:24 +0200

    Warnings removed

--- Util/qt/macOSarm64/lib/QtCore.framework/Versions/A/Headers/qpoint.h
+++ Util/qt/macOSarm64/lib/QtCore.framework/Versions/A/Headers/qpoint.h
@@ -356,8 +356,8 @@ QT_WARNING_DISABLE_INTEL(1572)
 
 Q_DECL_CONSTEXPR inline bool operator==(const QPointF &p1, const QPointF &p2)
 {
-    return ((!p1.xp || !p2.xp) ? qFuzzyIsNull(p1.xp - p2.xp) : qFuzzyCompare(p1.xp, p2.xp))
-        && ((!p1.yp || !p2.yp) ? qFuzzyIsNull(p1.yp - p2.yp) : qFuzzyCompare(p1.yp, p2.yp));
+    return ((p1.xp == 0 || p2.xp == 0) ? qFuzzyIsNull(p1.xp - p2.xp) : qFuzzyCompare(p1.xp, p2.xp))
+        && ((p1.yp == 0 || p2.yp == 0) ? qFuzzyIsNull(p1.yp - p2.yp) : qFuzzyCompare(p1.yp, p2.yp));
 }
 
 Q_DECL_CONSTEXPR inline bool operator!=(const QPointF &p1, const QPointF &p2)
