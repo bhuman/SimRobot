7c64125a55c2be6c2087bb1a9dbe9ad4b7d3406a
Thomas Röfer <Thomas.Roefer@dfki.de>
2021-11-26 17:34:18 +0100

    Create .lnk instead of symbolic link

--- Make/Windows/generate.cmd
+++ Make/Windows/generate.cmd
@@ -1,8 +1,7 @@
 @echo off
 pushd "%~dp0"
 cmake.exe -G "Visual Studio 17 2022" -A x64 -S ..\Common -B ..\..\Build\Windows\CMake
-if exist SimRobot.sln goto :linkExists
-if exist SimRobot.sln.lnk goto :linkExists
-mklink SimRobot.sln ..\..\Build\Windows\CMake\SimRobot.sln
+if exist SimRobot.lnk goto linkExists
+powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%CD%\SimRobot.lnk');$s.TargetPath='%CD%\..\..\Build\Windows\CMake\SimRobot.sln';$s.Save()"
 :linkExists
 popd
