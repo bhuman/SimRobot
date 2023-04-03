33e22aec65800e47842dedccf6b21959f2ad3c56
Thomas Röfer <Thomas.Roefer@dfki.de>
2023-04-03 10:20:54 +0200

    This does not seem to work anymore

--- Src/SimRobot/Main.cpp
+++ Src/SimRobot/Main.cpp
@@ -50,21 +50,6 @@ protected:
 
 /** Use the new class rather than the default one. */
 #define QApplication SimRobotApp
-
-/**
- * A helper for diplaying QStrings in Xcode that converts UTF16 strings to UTF8
- * character arrays that Xcode can display.
- * @param string The string to convert.
- * @return The address of the converted string. Note that it points to a static
- *         buffer that will be reused in the next call.
- */
-const char* _fromQString(const QString& string)
-{
-  static char buffer[1000];
-  strncpy(buffer, string.toUtf8().constData(), sizeof(buffer));
-  buffer[sizeof(buffer) - 1] = 0;
-  return buffer;
-}
 #endif // MACOS
 
 int main(int argc, char* argv[])
