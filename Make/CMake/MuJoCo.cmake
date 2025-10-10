21f0a47834a6b7d0fbaccdf8fc9ea8493f4922d1
Thomas Röfer <Thomas.Roefer@dfki.de>
2025-10-10 16:28:47 +0200

    Added win-MuJoCo without AVX

--- Make/CMake/MuJoCo.cmake
+++ Make/CMake/MuJoCo.cmake
@@ -1,9 +1,15 @@
 add_library(mujoco::mujoco SHARED IMPORTED)
 set_target_properties(mujoco::mujoco PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${SIMROBOT_PREFIX}/Util/MuJoCo/${PLATFORM}/include")
 if(WINDOWS)
-  set_target_properties(mujoco::mujoco PROPERTIES
-      IMPORTED_LOCATION "${SIMROBOT_PREFIX}/Util/MuJoCo/${PLATFORM}/bin/mujoco.dll"
-      IMPORTED_IMPLIB "${SIMROBOT_PREFIX}/Util/MuJoCo/${PLATFORM}/lib/mujoco.lib")
+  if(AVX)
+    set_target_properties(mujoco::mujoco PROPERTIES
+        IMPORTED_LOCATION "${SIMROBOT_PREFIX}/Util/MuJoCo/${PLATFORM}/bin/mujoco.dll"
+        IMPORTED_IMPLIB "${SIMROBOT_PREFIX}/Util/MuJoCo/${PLATFORM}/lib/mujoco.lib")
+  else()
+    set_target_properties(mujoco::mujoco PROPERTIES
+        IMPORTED_LOCATION "${SIMROBOT_PREFIX}/Util/MuJoCo/${PLATFORM}/bin-noavx/mujoco.dll"
+        IMPORTED_IMPLIB "${SIMROBOT_PREFIX}/Util/MuJoCo/${PLATFORM}/lib/mujoco.lib")
+  endif()
 elseif(MACOS)
   set_target_properties(mujoco::mujoco PROPERTIES IMPORTED_LOCATION "${SIMROBOT_PREFIX}/Util/MuJoCo/${PLATFORM}/lib/libmujoco.3.3.5.dylib")
 elseif(LINUX)
