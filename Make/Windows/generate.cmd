70c62f405d866ce31868e4c62d8b5a057158ca4a
Thomas Röfer <Thomas.Roefer@dfki.de>
2026-04-29 17:18:50 +0200

    Switch to VS2026

--- Make/Windows/generate.cmd
+++ Make/Windows/generate.cmd
@@ -1,7 +1,7 @@
 @echo off
 pushd "%~dp0"
-cmake.exe -G "Visual Studio 17 2022" -A x64 -S ..\CMake -B ..\..\Build\Windows\CMake
+cmake.exe -G "Visual Studio 18 2026" -A x64 -S ..\CMake -B ..\..\Build\Windows\CMake
 if exist SimRobot.lnk goto linkExists
-powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%CD%\SimRobot.lnk');$s.TargetPath='%CD%\..\..\Build\Windows\CMake\SimRobot.sln';$s.Save()"
+powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%CD%\SimRobot.lnk');$s.TargetPath='%CD%\..\..\Build\Windows\CMake\SimRobot.slnx';$s.Save()"
 :linkExists
 popd
